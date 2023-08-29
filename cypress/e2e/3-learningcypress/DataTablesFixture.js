f43d238d8573f02a873ae8a1d3bdd85a1ffc168a
Mustafa Babadagi <fathermountain@gmail.com>
2023-08-29 02:29:53 -0400

    version 8

---
+++ cypress/e2e/3-learningcypress/DataTablesFixture.js
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+
+ // Load user data from a JSON file
+   const userData = require('../../fixtures/userData.json');
+
+describe('Data-Driven Tests with Multiple Users', () => {
+      
+
+  beforeEach(() => {
+      cy.visit('https://www.bluerentalcars.com/login')
+      
+  });
+
+ 
+
+  userData.forEach((user) => {
+    it("Login as ${user.email} - ${user.scenario}", function()  {
+      cy.get('#formBasicEmail').type(user.email);
+      cy.get('#formBasicPassword').type(user.password);
+      cy.get("button[type='submit']").click();
+
+ 
+ 
+  });
+   });
+  });
+  
+    
+ 
---
+++ cypress/e2e/3-learningcypress/fixture.js
@@ -0,0 +1,40 @@
+/// <reference types="Cypress" />
+
+
+describe('This is fixture tutorials ', function() {
+
+    before('load',function() {
+       //Call the test data
+        
+        cy.fixture('datadriven').then(function (loginData) {
+
+            this.loginData = loginData
+
+        })
+        
+                
+    })
+
+
+    it('Learn data-driven testing or parameterizing a test', function()  {
+    
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+                
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(this.loginData.Username);
+
+         cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(this.loginData.Password);
+
+        cy.get('.oxd-button').click();
+        
+        
+        //To fix please change arrow functions () => { to non-arrow functions function() {.
+
+          //If using this you need to use function() (very very important).
+
+
+
+});
+
+    
+});
